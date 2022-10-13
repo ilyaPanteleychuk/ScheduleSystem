a9ab8038936b5226e050611a474bb51250e12c06
Ilya Panteleychuk <107109973+ilyaPanteleychuk@users.noreply.github.com>
2022-10-13 13:32:45 +0300

    Deleted old version of the project

--- UniversityScheduleSystem/src/main/java/com/ilyapanteleychuk/foxminded/universityschedule/dao/SubjectRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ilyapanteleychuk.foxminded.universityschedule.dao;
-
-import com.ilyapanteleychuk.foxminded.universityschedule.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-
-}
